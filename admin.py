ebb299519afc2dc9eb9e6951e27b9200767a8948
px57 <david@prepera.io>
2024-03-13 22:35:50 -0300

    refactor: update list_display formatting in admin.py
    The list_display attribute in the admin classes has been updated to follow a more readable formatting style. Each attribute is now on a new line inside a tuple.

--- admin.py
+++ admin.py
@@ -7,32 +7,46 @@ class CountriesAdmin(admin.ModelAdmin):
     """
     The countries admin.
     """
-    list_display = ('code',)
+    list_display = (
+        'code',
+    )
 
 @admin.register(models.Cities)
 class CitiesAdmin(admin.ModelAdmin):
     """
     The cities admin.
     """
-    list_display = ('name', 'latitude', 'longitude', 'country',)
+    list_display = (
+        'name', 
+        'latitude', 
+        'longitude', 
+        'country',
+    )
 
 @admin.register(models.Languages)
 class LanguagesAdmin(admin.ModelAdmin):
     """
     The languages admin.
     """
-    list_display = ('name', 'country',)
+    list_display = (
+        'name', 
+        'country',
+    )
 
 @admin.register(models.CountriesRelated)
 class CurrenciesAdmin(admin.ModelAdmin):
     """
     The currencies admin.
     """
-    list_display = ('country',)
+    list_display = (
+        'country',
+    )
 
 @admin.register(models.CitiesRelated)
 class CitiesRelatedAdmin(admin.ModelAdmin):
     """
     The cities related admin.
     """
-    list_display = ('city',)
+    list_display = (
+        'city',
+    )
