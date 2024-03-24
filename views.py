67e7f88a14277647408ed09367cd69c383077353
px57 <david@prepera.io>
2024-03-23 21:58:31 -0300

    feat: Add caching to city search API endpoint
    Why: To improve performance and reduce response time for the city search API endpoint.
    
    Changes:
    - Added import statement for cache_page decorator from django.views.decorators.cache
    - Added @cache_page(60 * 15) decorator to the find_city API endpoint to cache the response for 15 minutes.

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Q
+from django.views.decorators.cache import cache_page
+
 
 from kernel.http.decorators import load_response
 from kernel.forms.default_forms import AutocompleteForm
@@ -49,6 +51,7 @@ def find_city(
     ]
     return res.success()
 
+@cache_page(60 * 15)
 @load_response(
     stack=GEO_RULESTACK,
 )
